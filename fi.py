f1c28b27c7ce2a5d5d1091c513cd1df5837d14af
OskarsBest <30925882+OskarsBest@users.noreply.github.com>
2020-10-14 11:39:10 +0300

    ok
    kk

--- fi.py
+++ fi.py
@@ -1,5 +1,16 @@
-a = "Oskars Kipens"
-g2 = open("prove.txt","w",encoding="utf-8")
-for i in range(5):
-    print(i*" ",a,file=g2)
-g2.close()
+import math
+a = int(input("Mala a:"))
+b = int(input("Mala b:"))
+c = int(input("Mala c:"))
+if a>0. and b>0. and c>0.:
+    if a<(b+c) and b<(a+c) and c<(a+b):
+        g2 = open("prove.txt","w",encoding="utf-8")
+        print("VAR",file=g2)
+        g2.close()
+    else:
+        g2 = open("prove.txt","w",encoding="utf-8")
+        print("NEVAR",file=g2)
+        g2.close()
+
+      
+                    
