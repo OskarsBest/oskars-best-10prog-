4604a4701bd86093496b895701607ee267856e44
OskarsBest <30925882+OskarsBest@users.noreply.github.com>
2020-10-28 14:26:08 +0200

    Update fi.py

--- fi.py
+++ fi.py
@@ -1,16 +1,3 @@
-import math
-a = int(input("Mala a:"))
-b = int(input("Mala b:"))
-c = int(input("Mala c:"))
-if a>0. and b>0. and c>0.:
-    if a<(b+c) and b<(a+c) and c<(a+b):
-        g2 = open("prove.txt","w",encoding="utf-8")
-        print("VAR",file=g2)
-        g2.close()
-    else:
-        g2 = open("prove.txt","w",encoding="utf-8")
-        print("NEVAR",file=g2)
-        g2.close()
-
-      
-                    
+import datetime
+šodien = datetime.date.today().strftime("%d-%m-%Y")
+print(šodien)
